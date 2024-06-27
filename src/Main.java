39031489377064a08b1ca1385a1629e96de4400e
Zeke <ezequielgerstel777@gmail.com>
2024-06-27 16:07:30 -0400

    comments

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,12 @@ import java.util.Collections;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //stores the arrays of a random tone row, it's inverse and prepares the matrix to be used
         ArrayList <Integer> toneRow = defineToneRow();
         ArrayList <Integer> inverseRow = defineInverseRow(toneRow);
         ArrayList <ArrayList<Integer>> matrix = new ArrayList <>();
 
+        //this fills the matrix based on the tone row and it's transpositions, ordered by the inverse row
         for (int i = 0; i < 12; i++) {
             matrix.add(new ArrayList<>());
             matrix.set(i, defineTransponsedRow(toneRow, inverseRow.get(i)));
@@ -17,6 +19,7 @@ public class Main {
         }
         }
 
+    //it generates a random tone row
     public static ArrayList <Integer> defineToneRow(){
         ArrayList <Integer> toneRow= new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11));
         Collections.shuffle(toneRow);
@@ -24,6 +27,7 @@ public class Main {
     return toneRow;
     }
 
+    //it inverts the tone row provided
     public static ArrayList <Integer> defineInverseRow(ArrayList <Integer> toneRow){
         ArrayList <Integer> inverseRow = new ArrayList<>(toneRow);
         for (int i = 0; i < inverseRow.size(); i++){
@@ -34,6 +38,7 @@ public class Main {
         return inverseRow;
     }
 
+    //it transposes a given tone row the semitones or transposition specified
     public static ArrayList <Integer> defineTransponsedRow(ArrayList <Integer> toneRow, int transposition){
         ArrayList <Integer> transposedRow = new ArrayList<>(toneRow);
         for (int i = 0; i < transposedRow.size(); i++){
